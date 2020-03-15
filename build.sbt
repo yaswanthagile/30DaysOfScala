22ec7596176d955d154f3618e1a563e91a0e2e17
yaswanthagile <yaswanth.medisetti@gmail.com>
2020-03-15 19:29:44 +0530

    The obligatory "Hello World" program

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "30DaysOfScala"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
---
+++ src/main/scala/com/practice/HelloWorld.scala
@@ -0,0 +1,7 @@
+package com.practice
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello world")
+  }
+}
